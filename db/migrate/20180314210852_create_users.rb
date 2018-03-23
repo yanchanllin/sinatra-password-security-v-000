c422071cdc653edf5d434a95d0084fd2a9a83ff1
YanC Lin <yonalin1@gmail.com>
2018-03-23 21:05:08 +0000

    automatically backed up by learn

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
      t.string :username
      t.string :password_digest
-  end
+    end
+  end 
 
   def down
     drop_table :users
